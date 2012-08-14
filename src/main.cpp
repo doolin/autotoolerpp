1cb4a168f98746eebd9c8218434e7c215aeaa73f
Dave Doolin <david.doolin@gmail.com>
2012-08-14 09:06:38 -0700

    added unused parameter macros

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,14 @@
 #include <iostream>
 #include "autotoolerpp.h"
 
+/** Rename Unused Parameter */
+#define _UP(x) unused_p__ ## x
+
+/** Mark Unused Parameter */
+#define UP(x) _UP(x) __attribute__((unused))
+
 int
-main(int argc, char ** argv) {
+main(int __attribute__((unused))argc, char ** UP(argv)) {
 
   std::cout << get_some() << std::endl;
   return 0;
